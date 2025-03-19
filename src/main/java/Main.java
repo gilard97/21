700ad2c43479c7bf8c1f33e18cd2d4637c57cfc4
gilard97 <rurakkacper97@gmail.com>
2025-03-19 15:18:04 +0000

    2_1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,13 @@
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-
-// import org.junit.jupiter.api.Test;
+import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+    Scanner scanner = new Scanner(System.in);
+    System.out.println();
+    String input = scanner.nextLine();
+    String wynik = input.toLowerCase();
 
-  // @Test
-  // void addition() {
-  //     assertEquals(2, 1 + 1);
-  // }
+    System.out.println(wynik);
+      scanner.close();
+  }
 }
